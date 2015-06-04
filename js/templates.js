892af8a50efd7d69bb6ce86a56598a74fde8266a
Gavin Stark <gavin@gstark.com>
2015-06-04 14:40:39 -0400

    Adds semicolon and a comment

--- js/templates.js
+++ js/templates.js
@@ -56,7 +56,9 @@ $(document).ready(function()
     // -- use that to lookup the label, desc, and calories
     //    in the metadata using the value retrieved as the
     //    root of the key name.
-    var current_index = index
+
+    // Create a local scope variable to capture the index for use inside the closure below
+    var current_index = index;
     NEOCAST.data.player('item-' + current_index, function(item_key)
     {
       console.log("Setting up hook ups for item-" + current_index);
